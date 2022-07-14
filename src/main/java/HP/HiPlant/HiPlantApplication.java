79f2d1ad7736d17949ea24321e015dcf6fa8a3ed
Dong1333 <mogijisung13@gmail.com>
2022-07-14 18:17:00 +0900

    Initial commit

---
+++ src/main/java/HP/HiPlant/HiPlantApplication.java
@@ -0,0 +1,13 @@
+package HP.HiPlant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiPlantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiPlantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HP/HiPlant/HiPlantApplicationTests.java
@@ -0,0 +1,13 @@
+package HP.HiPlant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiPlantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
